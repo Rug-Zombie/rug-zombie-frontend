3347f5b5bb71a0cbfcd6c763fe6aa3fa53356b41
aubundy <atmb405@gmail.com>
2022-01-05 16:27:57 -0600

    feat: Add SectionHeader component

--- src/views/Home/components/NftSection/styles.ts
+++ src/views/Home/components/NftSection/styles.ts
@@ -5,23 +5,6 @@ export const NftContainer = styled.section`
   margin: 0 0 20px 0;
 `;
 
-export const NftTitle = styled.h2`
-  width: 100%;
-  text-align: center;
-  font: normal normal 600 36px/72px Poppins;
-  letter-spacing: 1.8px;
-  color: #FFFFFF;
-`;
-
-export const Hr = styled.hr`
-  width: 30px;
-  height: 5px;
-  background-color: #B8C00D;
-  border-radius: 3px;
-  border: 0px solid #B8C00D;
-  margin: 0 auto;
-`;
-
 export const NftCards = styled.div`
   display: flex;
   justify-content: space-around;
