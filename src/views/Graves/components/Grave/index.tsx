6e9635de06a0aa323cd06dc709dd930912689b3d
aubundy <atmb405@gmail.com>
2022-01-22 11:03:42 -0600

    refactor: Make GraveCard a styled component

--- src/views/Graves/components/Grave/index.tsx
+++ src/views/Graves/components/Grave/index.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-param-reassign */
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import downpointer from 'images/FullDownPointer.png'
-import GraveItem, { GraveItemType } from './components/Top/GraveItem'
-import { graveByPid } from '../../../../redux/get'
-import { Token } from '../../../../config/constants/types'
-import tokens from '../../../../config/constants/tokens'
 import Top from './components/Top'
-import GraveCard from './components/GraveCard'
 import Bottom from './components/Bottom'
 
+const GraveCard = styled.div<{ open: boolean }>`
+  width: 100%;
+  min-width: 260px;
+  min-height: 130px;
+  background-color: #151E21;
+  border-radius: 10px;
+  border: ${props => (props.open ? '2px solid #AE32AA' : '2px solid #151E21')};
+  padding: 20px;
+  margin: 0 0 20px 0;
+  display: flex;
+  flex-direction: column;
+`;
 
 interface GraveProps {
   pid: number
