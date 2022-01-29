8488473ce2580ce911bcb18044313b8fa6f4aba1
aubundy <atmb405@gmail.com>
2022-01-29 12:27:18 -0600

    feat: Make Filter responsive

--- src/views/Graveyard/components/Filter.tsx
+++ src/views/Graveyard/components/Filter.tsx
@@ -1,53 +1,61 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
   border: 2px solid #0D1417;
   border-radius: 25px;
   height: 50px;
+  min-width: 320px;
   display: flex;
   align-items: center;
-`
+  justify-content: center;
+  align-self: center;
+  margin: 25px 0;
+  &:hover { cursor: pointer; }
+`;
 
 const Selected = styled.div`
   height: 100%;
-  width: 50%;
-  min-width: 160px;
+  width: 160px;
   background: #30C00D 0% 0% no-repeat padding-box;
   border-radius: 25px;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const NotSelected = styled.div`
   height: 100%;
-  width: 50%;
-  min-width: 160px;
+  width: 160px;
   display: flex;
   align-items: center;
   justify-content: center;
   text-align: left;
   font: normal normal normal 14px/30px Poppins;
-  letter-spacing: 0px;
   color: #FFFFFF;
-  opacity: 1;
-`
+`;
 
 interface FilterProps {
   filters: string[];
   selected: number;
-  setSelected: any;
+  handleSelection: (index: any) => void;
 }
 
-const Filter: React.FC<FilterProps> = ({ filters, selected, setSelected }) => {
+const Filter: React.FC<FilterProps> = ({ filters, selected, handleSelection }) => {
+  const ButtonList = filters.map((filter, index) => {
+    const Button = index === selected ? Selected : NotSelected;
+    return (
+      <Button key={filter} onClick={() => {handleSelection(index)}}>
+        {filter}
+      </Button>
+    );
+  });
 
-  return <Container>
-    {filters.map((filter, index) => {
-      const Button = index === selected ? Selected : NotSelected
-      return <Button onClick={() => { setSelected(index)}}>{filter}</Button>
-    })}
-  </Container>
+  return (
+    <Container>
+      {ButtonList}
+    </Container>
+  );
 }
 
 export default Filter
