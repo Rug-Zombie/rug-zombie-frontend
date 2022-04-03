214cf7b9611fcbe92e23952181392f0ee04064d4
nnamdi <nnamdi.arimah@shopify.com>
2022-04-03 07:27:39 -0400

    fix: Inconsistent profile loading

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 import './Profile.Styles.css'
 import { Flex } from '@rug-zombie-libs/uikit'
+import { useWeb3React } from '@web3-react/core'
 import ProfilePage from './ProfilePage'
-import { account } from '../../redux/get'
 
 const Profile: React.FC = () => {
+  const { account } = useWeb3React()
   return (
     <>
-      {account() && account() !== '' ? (
+      {account ? (
         <ProfilePage />
       ) : (
         <Flex style={{ paddingTop: '10px', width: '100%', justifyContent: 'center' }}>
-          <div className="total-earned text-shadow">Connect Wallet to view your profile</div>
+          <div className='total-earned text-shadow'>Connect Wallet to view your profile</div>
         </Flex>
       )}
     </>
