074beb6bc87a83984d896e0cfbdd60c92c4aa889
Chaoticmaple <harrybrown@rogers.com>
2022-05-20 21:08:31 -0400

    fix: Homepage NFT cards are clickable and route to respective pages

--- src/components/Home/components/NftSection/index.tsx
+++ src/components/Home/components/NftSection/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import tombsLogo from 'images/home/Tombs.svg'
 import tombsArrow from 'images/home/TombArrow.svg'
 import gravesLogo from 'images/home/Graves.svg'
@@ -7,6 +8,7 @@ import spLogo from 'images/home/SpawningPools.svg'
 import spArrow from 'images/home/SpawningPoolArrow.svg'
 import SectionContainer from '../SectionContainer'
 import SectionHeader from '../SectionHeader'
+
 
 import {
   NftCards,
@@ -19,7 +21,8 @@ import {
   NftCardTextDiv,
   NftCardText,
   NftCardFooter,
-  NftCardFooterAction,
+  NftCardCursorHover,
+  NFTCardTextRight,
 } from './styles'
 
 interface NftSectionProps {
@@ -31,76 +34,84 @@ const NftSection: React.FC<NftSectionProps> = ({ history }) => {
     <SectionContainer backgroundColor="#010202">
       <SectionHeader title="Farm NFTs in multiple ways" />
       <NftCards>
+
+
         <NftCard>
-          <HalfLine color="#AE32AA" />
-          <NftCardImg src={gravesLogo} alt="Graves Logo" />
-          <NftCardContent>
-            <NftCardTitleText>Graves</NftCardTitleText>
-            <NftCardSubText>Create NFTs from the dead tokens lingering in your wallet</NftCardSubText>
-            <NftCardText>
+          <Link to='/graves'>
+            <NftCardCursorHover>
+            <HalfLine color="#AE32AA" />
+            <NftCardImg src={gravesLogo} alt="Graves Logo" />
+            <NftCardContent>
+              <NftCardTitleText>Graves</NftCardTitleText>
+              <NftCardSubText>Create NFTs from the dead tokens lingering in your wallet</NftCardSubText>
+              <NftCardText>
               Unlock graves using rugged tokens in your wallet to earn NFTs on top of regular ZMBE rewards
-            </NftCardText>
-          </NftCardContent>
-          <NftCardFooter>
-            <NftCardFooterAction
-              onClick={() => {
-                history.push('/graves')
-              }}
-            >
-              <NftCardTextDiv>
-                <NftCardText color="#AE32AA">enter graves </NftCardText>
-                <img src={gravesArrow} alt="Grave Card Arrow" />
-              </NftCardTextDiv>
-            </NftCardFooterAction>
-          </NftCardFooter>
+              </NftCardText>
+            </NftCardContent>
+
+            <NftCardFooter>
+              <NFTCardTextRight color="#AE32AA">enter graves
+              <img src={gravesArrow} alt="Grave Card Arrow" />
+              </NFTCardTextRight>
+            </NftCardFooter>
+
+            </NftCardCursorHover>
+          </Link>
         </NftCard>
+
+
+
         <NftCard>
-          <HalfLine color="#4B7BDC" />
-          <NftCardImg src={tombsLogo} alt="Tombs Logo" />
-          <NftCardContent>
-            <NftCardTitleText>Tombs</NftCardTitleText>
-            <NftCardSubText>Boost NFT rarity by providing liquidity</NftCardSubText>
-            <NftCardText>
-              Roll for NFTs of varying rarity while earning ZMBE by staking LP tokens. Increase your odds by increasing
-              your stake in the tombs
-            </NftCardText>
-          </NftCardContent>
-          <NftCardFooter>
-            <NftCardFooterAction
-              onClick={() => {
-                history.push('/tombs')
-              }}
-            >
-              <NftCardTextDiv>
-                <NftCardText color="#4B7BDC">enter tombs </NftCardText>
+          <Link to='/tombs'>
+            <NftCardCursorHover>
+            <HalfLine color="#4B7BDC" />
+            <NftCardImg src={tombsLogo} alt="Tombs Logo" />
+            <NftCardContent>
+              <NftCardTitleText>Tombs</NftCardTitleText>
+              <NftCardSubText>Boost NFT rarity by providing liquidity</NftCardSubText>
+              <NftCardText>
+                Roll for NFTs of varying rarity while earning ZMBE by staking LP tokens. Increase your odds by increasing
+                your stake in the tombs
+              </NftCardText>
+            </NftCardContent>
+
+            <NftCardFooter>
+              <NFTCardTextRight color="#4B7BDC">enter tombs
                 <img src={tombsArrow} alt="Tomb Card Arrow" />
-              </NftCardTextDiv>
-            </NftCardFooterAction>
-          </NftCardFooter>
+              </NFTCardTextRight>
+            </NftCardFooter>
+
+            </NftCardCursorHover>
+          </Link>
         </NftCard>
+
+
+
         <NftCard>
-          <HalfLine color="#30C00D" />
-          <NftCardImg src={spLogo} alt="Spawning Pools Logo" />
-          <NftCardContent>
-            <NftCardTitleText>Spawning Pools</NftCardTitleText>
-            <NftCardSubText>Earn NFTs and returns in partnered project tokens</NftCardSubText>
-            <NftCardText>
-              Farm NFTs by staking ZMBE, while earning rewards in tokens from RugZombie partners
-            </NftCardText>
-          </NftCardContent>
-          <NftCardFooter>
-            <NftCardFooterAction
-              onClick={() => {
-                history.push('/spawning_pools')
-              }}
-            >
-              <NftCardTextDiv>
-                <NftCardText color="#30C00D">enter spawning pools </NftCardText>
-                <img src={spArrow} alt="Spawning Pool Card Arrow" />
-              </NftCardTextDiv>
-            </NftCardFooterAction>
-          </NftCardFooter>
+          <Link to='/spawning_pools'>
+            <NftCardCursorHover>
+              <HalfLine color="#30C00D" />
+              <NftCardImg src={spLogo} alt="Spawning Pools Logo" />
+              <NftCardContent>
+                <NftCardTitleText>Spawning Pools</NftCardTitleText>
+                <NftCardSubText>Earn NFTs and returns in partnered project tokens</NftCardSubText>
+                <NftCardText>
+                  Farm NFTs by staking ZMBE, while earning rewards in tokens from RugZombie partners
+                </NftCardText>
+              </NftCardContent>
+
+              <NftCardFooter>
+                <NFTCardTextRight color="#30C00D">enter spawning pools
+                  <img src={spArrow} alt="Spawning Pool Card Arrow" />
+                </NFTCardTextRight>
+              </NftCardFooter>
+
+            </NftCardCursorHover>
+          </Link>
         </NftCard>
+
+
+        
       </NftCards>
     </SectionContainer>
   )
